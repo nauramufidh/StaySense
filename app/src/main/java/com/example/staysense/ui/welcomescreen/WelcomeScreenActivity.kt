bc9b906917376e306a91f19059b48d8d0f15e42b
kasanindit <kasanindit>
2025-03-28 14:03:54 +0700

    update welcome

--- app/src/main/java/com/example/staysense/ui/welcomescreen/WelcomeScreenActivity.kt
+++ app/src/main/java/com/example/staysense/ui/welcomescreen/WelcomeScreenActivity.kt
@@ -1,23 +1,46 @@
 package com.example.staysense.ui.welcomescreen
 
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.staysense.R
+import com.example.staysense.databinding.ActivityWelcomeScreenBinding
 
 class WelcomeScreenActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityWelcomeScreenBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_welcome_screen)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        binding = ActivityWelcomeScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        
+        setupaction()
+
+    }
+
+    private fun setupaction() {
+        binding.btnSignup.setOnClickListener {
+//            val intentToLogin = Intent(this, LoginActivity::class.java)
+//            intentToLogin.putExtra("fromRegister", true)
+//            startActivity(intentToLogin)
+//            finish()
+        }
+    }
+
+    private fun playAnimation() {
+        ObjectAnimator.ofFloat(binding.ivWelcomeIcon, View.TRANSLATION_X, -30f, 30f).apply {
+            duration = 6000
+            repeatCount = ObjectAnimator.INFINITE
+            repeatMode = ObjectAnimator.REVERSE
+        }.start()
+
     }
 }
