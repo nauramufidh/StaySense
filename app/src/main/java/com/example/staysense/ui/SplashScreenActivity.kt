f031880377f2ccb8537498b5fce8c1aac2f599e3
nauramufidh <nauramufidah274@gmail.com>
2025-03-28 11:04:23 +0700

    splash screen

---
+++ app/src/main/java/com/example/staysense/ui/SplashScreenActivity.kt
@@ -0,0 +1,20 @@
+package com.example.staysense.ui
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+import com.example.staysense.MainActivity
+import com.example.staysense.R
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+        }, 3000)
+    }
+}
