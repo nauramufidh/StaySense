fd9a2e331c6e8d85dbf71b4355e04bc2ea448bc6
kasanindit <kasanindit>
2025-03-28 12:36:08 +0700

    add icon email and lock

---
+++ app/src/main/java/com/example/staysense/ui/welcomescreen/WelcomeScreenActivity.kt
@@ -0,0 +1,21 @@
+package com.example.staysense.ui.welcomescreen
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.staysense.R
+
+class WelcomeScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_welcome_screen)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
