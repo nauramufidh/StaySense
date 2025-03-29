3de276cbea43716d36537153748765243b2e3e18
kasanindit <kasanindit>
2025-03-29 16:31:08 +0700

    add pie chart

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,6 +14,7 @@ android {
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
+        multiDexEnabled = true
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
@@ -52,4 +53,7 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation (libs.anychart.android)
+    implementation (libs.multidex)
 }
--- app/src/main/java/com/example/staysense/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/staysense/ui/home/HomeFragment.kt
@@ -5,56 +5,72 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import com.anychart.AnyChart
+import com.anychart.chart.common.dataentry.ValueDataEntry
+import com.anychart.chart.common.listener.Event
+import com.anychart.chart.common.listener.ListenersInterface
+import com.anychart.enums.Align
+import com.anychart.enums.LegendLayout
 import com.example.staysense.R
+import com.example.staysense.databinding.FragmentHomeBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [HomeFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class HomeFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment HomeFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            HomeFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setupPieChart()
+    }
+
+    private fun setupPieChart() {
+        val anyChartView = binding.churnPiechart
+        anyChartView.setProgressBar(binding.progressbar)
+
+        val pie = AnyChart.pie()
+
+        pie.setOnClickListener(object : ListenersInterface.OnClickListener(arrayOf("x", "value")) {
+            override fun onClick(event: Event?) {
+                if (event != null) {
+                    Toast.makeText(requireContext(), "${event.getData()["x"]}: ${event.getData()["value"]}", Toast.LENGTH_SHORT).show()
                 }
             }
+        })
+
+        val data = listOf(
+            ValueDataEntry("Yes", 6371664),
+            ValueDataEntry("No", 789622),
+        )
+
+        pie.data(data)
+        pie.title("Churn Pie Chart")
+        pie.labels().position("outside")
+
+//        pie.legend().title().enabled(true)
+//        pie.legend().title().text("Churn").padding(0.0, 0.0, 10.0, 0.0)
+
+        pie.legend()
+            .position("right")
+            .itemsLayout(LegendLayout.VERTICAL)
+            .align(Align.CENTER)
+
+        anyChartView.setChart(pie)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
