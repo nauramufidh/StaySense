04226bfb94c0079b1d97b9cba4d3c7de5d79753e
nauramufidh <nauramufidah274@gmail.com>
2025-04-22 09:40:42 +0700

    wordcloud blm inputan

--- app/src/main/java/com/example/staysense/ui/wordcloud/WordCloudFragment.kt
+++ app/src/main/java/com/example/staysense/ui/wordcloud/WordCloudFragment.kt
@@ -4,11 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.anychart.AnyChart
 import com.anychart.AnyChartView
 import com.anychart.chart.common.dataentry.CategoryValueDataEntry
 import com.anychart.chart.common.dataentry.DataEntry
+import com.anychart.chart.common.dataentry.ValueDataEntry
 import com.anychart.charts.TagCloud
 import com.anychart.scales.OrdinalColor
 import com.example.staysense.databinding.FragmentWordCloudBinding
@@ -51,15 +53,38 @@ class WordCloudFragment : Fragment() {
 
         tagCloud.colorRange().enabled(true)
         tagCloud.colorRange().colorLineSize(15.0)
+        tagCloud.background().fill("#F6F8D5")
 
-        val data = listOf(
-            CategoryValueDataEntry("China", "asia", 1383220000),
-            CategoryValueDataEntry("India", "asia", 1316000000),
-            CategoryValueDataEntry("United States", "america", 324982000),
-            CategoryValueDataEntry("Indonesia", "asia", 263510000),
+        val rawData = listOf(
+            CategoryValueDataEntry("China", "asia", 1),
+            CategoryValueDataEntry("India", "asia", 1),
+            CategoryValueDataEntry("India", "asia", 1),
+            CategoryValueDataEntry("India", "asia", 1),
+            CategoryValueDataEntry("India", "asia", 1),
+            CategoryValueDataEntry("United States", "america", 1),
+            CategoryValueDataEntry("Indonesia", "asia", 1),
+            CategoryValueDataEntry("Indonesia", "asia", 1),
+            CategoryValueDataEntry("Indonesia", "asia", 1),
+            CategoryValueDataEntry("Indonesia", "asia", 1),
+            CategoryValueDataEntry("Indonesia", "asia", 1),
+            CategoryValueDataEntry("Korea", "asia", 1),
+            CategoryValueDataEntry("New York", "america", 1)
         )
 
-        tagCloud.data(data as List<DataEntry>)
+        val groupedData = rawData.groupBy { it.getValue("x") as String }
+            .map { (country, entries) ->
+                val region = entries.first().getValue("category") as String
+                val totalCount = entries.sumOf { it.getValue("value").toString().toInt() }
+                CategoryValueDataEntry(country, region, totalCount)
+            }
+
+        tagCloud.data(groupedData as List<DataEntry>)
         anyChartView.setChart(tagCloud)
+    }
+
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
