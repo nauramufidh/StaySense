f39c4e8902ab2739348d219996f77701aee1dab2
kasanindit <kasanindit@gmail.com>
2025-05-23 21:57:34 +0700

    update ChartResponse.kt

--- app/src/main/java/com/example/staysense/data/response/ChartResponse.kt
+++ app/src/main/java/com/example/staysense/data/response/ChartResponse.kt
@@ -10,8 +10,27 @@ data class ChartResponse(
 	@field:SerializedName("bar_chart")
 	val barChart: List<BarChartItem?>? = null,
 
+	@field:SerializedName("information")
+	val information: Information? = null,
+
 	@field:SerializedName("pie_chart")
 	val pieChart: PieChart? = null
+) : Parcelable
+
+@Parcelize
+data class Information(
+
+	@field:SerializedName("average_churn_rate")
+	val averageChurnRate: String? = null,
+
+	@field:SerializedName("total_customers")
+	val totalCustomers: Int? = null,
+
+	@field:SerializedName("total_not_churn")
+	val totalNotChurn: Int? = null,
+
+	@field:SerializedName("total_churn")
+	val totalChurn: Int? = null
 ) : Parcelable
 
 @Parcelize
